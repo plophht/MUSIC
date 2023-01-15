fd749851e0539edc24407df584b1e5caae79fd7c
plophht <122690619+plophht@users.noreply.github.com>
2023-01-15 15:47:36 +0200

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,15 +57,15 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYM")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("FTTUTY")
+                await self.one.join_chat("PTTUTT1")
+                await self.one.join_chat("FTTUTT")
             except:
                 pass
             assistants.append(1)
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "تم تنصيب المساعد علي سورس سيمو @FTTUTY "
                 )
             except:
                 LOGGER(__name__).error(
@@ -88,15 +88,15 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYM")
-                await self.two.join_chat("TheYukki")
-                await self.two.join_chat("YukkiSupport")
+                await self.two.join_chat("PTTUTT1")
+                await self.two.join_chat("FTTUTY")
+                await self.two.join_chat("FTTUTT")
             except:
                 pass
             assistants.append(2)
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "تم تنصيب المساعد علي سورس سيمو @FTTUTY"
                 )
             except:
                 LOGGER(__name__).error(
@@ -119,15 +119,15 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYM")
-                await self.three.join_chat("TheYukki")
-                await self.three.join_chat("YukkiSupport")
+                await self.three.join_chat("PTTUTT1")
+                await self.three.join_chat("FTTUTY")
+                await self.three.join_chat("FTTUTT")
             except:
                 pass
             assistants.append(3)
             try:
                 await self.three.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "تم تنصيب المساعد علي سورس سيمو @FTTUTY"
                 )
             except:
                 LOGGER(__name__).error(
@@ -150,15 +150,15 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYM")
-                await self.four.join_chat("TheYukki")
-                await self.four.join_chat("YukkiSupport")
+                await self.four.join_chat("FTTUTY")
+                await self.four.join_chat("FTTUTT")
+                await self.four.join_chat("PTTUTT1")
             except:
                 pass
             assistants.append(4)
             try:
                 await self.four.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "تم تنصيب المساعد علي سورس سيمو @FTTUTY "
                 )
             except:
                 LOGGER(__name__).error(
@@ -189,7 +189,7 @@ class Userbot(Client):
             assistants.append(5)
             try:
                 await self.five.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "تم تنصيب المساعد علي سورس سيمو @FTTUTY "
                 )
             except:
                 LOGGER(__name__).error(
