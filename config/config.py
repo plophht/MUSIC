d17d4b13646853a32e9ba0ac730b5a1251597b26
plophht <122690619+plophht@users.noreply.github.com>
2023-01-15 05:33:52 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -44,7 +44,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "5680297831","5957205447").split()) 
+    map(int, getenv("OWNER_ID", "5680297831").split()) 
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
