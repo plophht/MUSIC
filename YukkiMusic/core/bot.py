5c7288be804a29d54f924c19ad34141b4030493d
plophht <122690619+plophht@users.noreply.github.com>
2023-01-15 15:47:50 +0200

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -34,7 +34,7 @@ class YukkiBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "تم تنصيب بوتك علي سورس سيمو @FTTUTY 🔥"
+                config.LOG_GROUP_ID, "تم تنصيب بوتك علي سورس سيمو @FTTUTY "
             )
         except:
             LOGGER(__name__).error(
